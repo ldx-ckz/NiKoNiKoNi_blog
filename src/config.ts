75144b22622b0afe5de8e056dc938f29196e1394
NiKo <1951513866qq@gmail.com>
2026-07-25 22:06:46 +0800

    fix: support an empty post collection

--- src/config.ts
+++ src/config.ts
@@ -23,7 +23,7 @@ export const siteConfig: SiteConfig = {
 	title: "nikonikoni's blog",
 	subtitle: "a blog website",
 	siteURL: "https://miku.nikonikoni.blog/", // 请替换为你的站点URL，以斜杠结尾
-	siteStartDate: "2026-7-25", // 站点开始运行日期，用于站点统计组件计算运行天数
+	siteStartDate: "2026-07-25", // 站点开始运行日期，用于站点统计组件计算运行天数
 
 
 	timeZone: SITE_TIMEZONE,
