665bfd20945a2f16f42124053e1ba3deb2c1038c
NiKo <1951513866qq@gmail.com>
2026-08-18 15:31:16 +0800

    modified the BANNER_HEIGHT_EXTEND

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,7 +6,7 @@ export const DEFAULT_THEME = LIGHT_MODE;
 
 // Banner height unit: vh
 export const BANNER_HEIGHT = 35;
-export const BANNER_HEIGHT_EXTEND = 30;
+export const BANNER_HEIGHT_EXTEND = 35;
 export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;
 
 // The height the main panel overlaps the banner, unit: rem
